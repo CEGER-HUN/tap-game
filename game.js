82b2827747ea9623dcddd61f7dc9664bac67afff
CEGER-HUN <91566050+CEGER-HUN@users.noreply.github.com>
2026-01-22 22:32:43 +0300

    Delete game.js

--- game.js
+++
@@ -1,9 +0,0 @@
-let coins = 0;
-
-const counter = document.getElementById("counter");
-const coin = document.getElementById("coin");
-
-coin.addEventListener("click", () => {
-  coins++;
-  counter.innerText = "Coins: " + coins;
-});
